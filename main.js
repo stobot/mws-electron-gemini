5f55a5bb4eb5ab2ea71992483c512f3886ef2e26
stobot <39247173+stobot@users.noreply.github.com>
2025-08-15 13:56:42 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,36 +1,44 @@
 const { app, BrowserWindow } = require('electron');
 const { exec } = require('child_process');
 
-// Start the TiddlyWiki server in the background.
-const tiddlywiki = exec('npm start');
+// This correctly starts the MWS server via the npm script.
+const mwsServer = exec('npm start');
 
 function createWindow () {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
-    // We can remove the webPreferences as they are not needed for this approach.
   });
 
-  // Load the URL of our local TiddlyWiki server.
-  win.loadURL('http://127.0.0.1:8888');
+  // The MWS server uses port 8080 by default. This is a key change.
+  win.loadURL('http://127.0.0.1:8080');
 }
 
-// Wait for the Electron app itself to be ready.
-app.whenReady().then(() => {
-  // Now, listen to the server's output stream.
-  tiddlywiki.stdout.on('data', (data) => {
-    // When the output includes the text "Serving on", we know the server is ready.
-    if (data.includes('Serving on')) {
-      // Only now is it safe to create the display window.
+// Listen for output from the server process.
+mwsServer.stdout.on('data', (data) => {
+  // The MWS server outputs "listening on port" when it's ready.
+  // This is the second key change.
+  if (data.includes('listening on port')) {
+    // As soon as we see that message, we create the window.
+    // We add a check to make sure we don't accidentally open multiple windows.
+    if (BrowserWindow.getAllWindows().length === 0) {
       createWindow();
     }
-  });
+  }
 });
 
-// This part remains the same: clean up when all windows are closed.
+// It's also good practice to log any errors.
+mwsServer.stderr.on('data', (data) => {
+  console.error(`MWS Server Error: ${data}`);
+});
+
+// We don't need anything inside app.whenReady() because the
+// stdout listener above will trigger the window creation.
+app.whenReady();
+
+// Make sure we shut down the server when the app closes.
 app.on('window-all-closed', () => {
-  // Important: kill the server process when the app closes.
-  tiddlywiki.kill();
+  mwsServer.kill();
   if (process.platform !== 'darwin') {
     app.quit();
   }
